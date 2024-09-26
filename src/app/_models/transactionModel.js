c0496963e9cf3dfe711b0bacd69aa79c158a60dd
kj139 <therealkjai139@gmail.com>
2024-09-26 01:21:59 -0400

    start auth setup

---
+++ src/app/_models/transactionModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+const TransactionModel = new Schema({
+    userId: {
+        type:Schema.ObjectId,
+        ref:'User'
+    },
+    amount: {
+        type:Number,
+        required:true
+    },
+    transactionType: {
+        type:String,
+        enum: ['purchase', 'refund'],
+        required:true
+    }
+}, {
+    timestamps: true
+})
+
+
+export default mongoose.models.Transaction || mongoose.model('Transaction', TransactionModel)
---
+++ src/app/_models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+
+const UserModel = new Schema({
+    name: {
+        type:String,
+        unique:true,
+    },
+    email: {
+        type:String,
+        required:true,
+        unique:true
+    },
+    currencyAmt: {
+        type:Number,
+        default: 0
+    }
+})
+
+
+export default mongoose.models.User || mongoose.model('User', UserModel)
