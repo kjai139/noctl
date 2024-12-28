08d43dc0a555595c07ba938747911db4a8b10375
kj139 <therealkjai139@gmail.com>
2024-12-28 03:28:46 -0500

    update css load ani

--- src/app/loading..tsx
+++ src/app/loading..tsx
@@ -1,9 +1,12 @@
-
+import Image from "next/image"
+import siteLogo from '../../public/mmtlicon.png'
 export default function Loading() {
 
     return (
-        <div>
-            Loading serverside...
+        <div className="items-center justify-center h-screen w-full flex">
+            <div className="icon-load">
+                <Image src={siteLogo} height={80} width={80} alt="Site logo"></Image>
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,57 +9,57 @@ import { auth } from "../../auth";
 import { Suspense } from "react";
 import Loading from "./loading.";
 
-async function getProductList () {
+async function getProductList() {
   try {
-      const products = await stripeInstance.products.list({
-          expand:['data.default_price']
-      })
+    const products = await stripeInstance.products.list({
+      expand: ['data.default_price']
+    })
 
-      if (products) {
-          return products
-      } else {
-          return null
-      }
-  } catch (err) {
-      console.error('Error getting products', err)
+    if (products) {
+      return products
+    } else {
       return null
+    }
+  } catch (err) {
+    console.error('Error getting products', err)
+    return null
   }
 }
 
 async function Home() {
   const products = await getProductList()
-      console.log('Products from stripe:', products)
-      let plainProducts
-      if (products && products.data) {
-          plainProducts = products.data.map((node) => {
-              return {
-                  id:node.id,
-                  defaultPrice: node.default_price,
-                  images: node.images,
-                  name:node.name,
-                  description: node.description
-              }
-  
-          })
+  console.log('Products from stripe:', products)
+  let plainProducts
+  if (products && products.data) {
+    plainProducts = products.data.map((node) => {
+      return {
+        id: node.id,
+        defaultPrice: node.default_price,
+        images: node.images,
+        name: node.name,
+        description: node.description
       }
-      console.log('plainproducts', plainProducts)
-      const session = await auth()
+
+    })
+  }
+  console.log('plainproducts', plainProducts)
+  const session = await auth()
   return (
     <>
-    <AppSidebar></AppSidebar>
-    <div className="flex flex-col w-full h-auto flex-1">
-      <TopNav products={plainProducts} session={session}></TopNav>
-      <TopNavInfo></TopNavInfo>
-      <main className="flex main-p flex-col items-center w-full">
-        <div className="flex flex-col gap-8 px-4 w-full mw">
-          <MainInputForm></MainInputForm>
-          <ResultRender></ResultRender>
+      <AppSidebar></AppSidebar>
+      <div className="flex flex-col w-full h-auto flex-1">
+        <TopNav products={plainProducts} session={session}></TopNav>
+        <TopNavInfo></TopNavInfo>
+        <main className="flex main-p flex-col items-center w-full">
+          <div className="flex flex-col gap-8 px-4 w-full mw">
+            <MainInputForm></MainInputForm>
+            <ResultRender></ResultRender>
+          </div>
+        </main>
+        <div>
+          <SidebarExpandBtn className="rounded-full h-auto w-auto p-4"></SidebarExpandBtn>
         </div>
-      </main>
-      <div>
-        <SidebarExpandBtn className="rounded-full h-auto w-auto p-4"></SidebarExpandBtn>
       </div>
-    </div>
     </>
   );
 }
@@ -73,6 +73,7 @@ export default function Page() {
 
         </Home>
       </Suspense>
+
     </>
   )
 }
